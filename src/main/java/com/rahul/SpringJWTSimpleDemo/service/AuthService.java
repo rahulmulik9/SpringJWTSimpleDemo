674d78d58c6514a8242476e81006e13e6a933415
Rahul Mulik <rahulmulik9@gmail.com>
2026-08-20 12:41:30 +0530

    Refactored

--- src/main/java/com/rahul/SpringJWTSimpleDemo/service/AuthService.java
+++ src/main/java/com/rahul/SpringJWTSimpleDemo/service/AuthService.java
@@ -24,35 +24,6 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
-    //    public String register(RegisterRequest request) {
-//        if (userRepository.existsByUsername(request.getUsername())) {
-//            throw new IllegalArgumentException("Username already taken");
-//        }
-//
-//        Role role = Role.valueOf(request.getRole().toUpperCase());
-//
-//        User user = User.builder()
-//                .username(request.getUsername())
-//                .password(passwordEncoder.encode(request.getPassword()))
-//                .role(role)
-//                .build();
-//
-//        userRepository.save(user);
-//
-//        return "User registered successfully with username: " + user.getUsername();
-//    }
-//
-//    public AuthResponse login(LoginRequest request) {
-//        authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
-//        );
-//
-//        User user = userRepository.findByUsername(request.getUsername())
-//                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-//
-//        String token = jwtUtil.generateToken(user.getUsername(), user.getRole().name());
-//        return new AuthResponse(token, user.getUsername(), user.getRole().name());
-//    }
     public String register(RegisterRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) {
             throw new DuplicateUsernameException(request.getUsername());

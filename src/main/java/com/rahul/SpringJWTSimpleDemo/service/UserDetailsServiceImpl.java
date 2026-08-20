e2bcb15099b68fa8a6fc6f80fceaadd19adc0f5f
Rahul Mulik <rahulmulik9@gmail.com>
2026-08-20 11:49:05 +0530

    Added JWT auth filter

---
+++ src/main/java/com/rahul/SpringJWTSimpleDemo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.rahul.SpringJWTSimpleDemo.service;
+
+import com.rahul.SpringJWTSimpleDemo.repository.UserRepository;
+import com.rahul.SpringJWTSimpleDemo.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}

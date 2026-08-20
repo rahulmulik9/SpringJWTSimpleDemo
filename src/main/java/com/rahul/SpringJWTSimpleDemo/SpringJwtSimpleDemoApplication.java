15f2e058ec5db9ac29cd58c5d038cb0f325c6d90
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-08-20 11:33:23 +0530

    Initial commit

---
+++ src/main/java/com/rahul/SpringJWTSimpleDemo/SpringJwtSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.SpringJWTSimpleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSimpleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSimpleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/SpringJWTSimpleDemo/SpringJwtSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.SpringJWTSimpleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtSimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
